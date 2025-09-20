84efa6bdd335eb80215cfc3e2796e11da8d8961e
Cahyo Permono <cahyo.adi@imaniprima.com>
2025-09-20 07:19:42 +0700

    `feat(video): modified jsContent replacement and content type in video feature`

--- src/features/video/index.ts
+++ src/features/video/index.ts
@@ -582,6 +582,11 @@ app.openapi(hwWebsocketRoute, async (c) => {
     }
     
     let jsContent = await response.text()
+
+    jsContent = jsContent.replace(
+      /document\.location\.hostname/g,
+      `'vss.gtrack.co.id'`
+    )
     
     // Set the correct content type for JavaScript
     c.header('Content-Type', 'application/javascript')
